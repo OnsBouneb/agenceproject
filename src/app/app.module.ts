2cff7937f6cf066b87c3d7ce61b1972001ce6b11
Ons Bouneb <onsbouneb1@gmail.com>
2019-01-02 16:14:30 +0100

    Initial comment14

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';import { AppComponent } from './app.component';
+import { VolComponent } from './vol/vol.component';
+import {VolService} from './vol.service';
+import {HebergementService}from './hebergement.service';
+import { Routes,RouterModule } from '@angular/router';
+import { HebergementComponent } from './hebergement/hebergement.component';
+import { VilleComponent } from './ville/ville.component';
+import{VilleService} from './ville.service';
+import { TransportComponent } from './transport/transport.component';
+import {TransportService} from './transport.service';
+import { from } from 'rxjs';
+const appRoutes: Routes = [
+  { path: 'vols', component: VolComponent },
+  { path: 'hebergements', component: HebergementComponent },
+  { path: 'villes', component: VilleComponent },
+  { path: 'transports', component: TransportComponent },
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VolComponent,
+    HebergementComponent,
+    VilleComponent,
+    TransportComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [VolService,HebergementService,VilleService,TransportService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
